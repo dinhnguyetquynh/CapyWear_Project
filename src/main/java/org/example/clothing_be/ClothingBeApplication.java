245a0c4e54dbd5121f051ecdeaa4257697bad094
dinhnguyetquynh <dquynhd202@gmail.com>
2026-04-24 11:32:52 +0700

    build war

--- src/main/java/org/example/clothing_be/ClothingBeApplication.java
+++ src/main/java/org/example/clothing_be/ClothingBeApplication.java
@@ -2,9 +2,15 @@ package org.example.clothing_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ClothingBeApplication {
+public class ClothingBeApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ClothingBeApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ClothingBeApplication.class, args);
