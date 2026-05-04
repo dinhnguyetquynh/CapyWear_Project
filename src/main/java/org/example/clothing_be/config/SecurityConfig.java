5fbf612d7a05e5842e6edf93aeda69e086db4274
Quynh Dinh Nguyet <dquynhd202@gmail.com>
2026-05-04 13:28:55 +0700

    config security

--- src/main/java/org/example/clothing_be/config/SecurityConfig.java
+++ src/main/java/org/example/clothing_be/config/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/public/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/item").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/item/{itemId}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/item/search/suggest/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
