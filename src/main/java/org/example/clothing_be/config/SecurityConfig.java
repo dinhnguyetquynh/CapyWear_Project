bbe67ad6166ccf5bb2c1e211dce091b67449d093
Quynh Dinh Nguyet <dquynhd202@gmail.com>
2026-05-05 14:06:34 +0700

    add api health check

--- src/main/java/org/example/clothing_be/config/SecurityConfig.java
+++ src/main/java/org/example/clothing_be/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/item").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/item/{itemId}").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/item/search/suggest/**").permitAll()
+                        .requestMatchers("/health").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
