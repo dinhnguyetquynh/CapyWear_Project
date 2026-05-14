e8cf1c5f6c5d33865c325c79845f0d27e20d6bab
Quynh Dinh Nguyet <dquynhd202@gmail.com>
2026-05-14 09:21:16 +0700

    build war

--- src/main/java/org/example/clothing_be/config/SecurityConfig.java
+++ src/main/java/org/example/clothing_be/config/SecurityConfig.java
@@ -45,13 +45,13 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/public/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/item").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/item/{itemId}").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/item/search/suggest/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint((request, response, authException) -> {
